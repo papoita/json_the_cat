49332b5507ec2022cca748778840cfa4bf624f1f
PaolaP <paolaperez8@gmail.com>
2021-11-21 00:26:16 -0500

    linted

--- breedFetcher.js
+++ breedFetcher.js
@@ -2,7 +2,6 @@
 /**
  * @TODO handle errors
  */
-const net = require("net");
 
 // 20211120001403
 // https://api.thecatapi.com/v1/breeds/search
@@ -19,22 +18,22 @@ const urlInput = `https://api.thecatapi.com/v1/breeds/search?q=${process.argv[2]
 const path = process.argv[3];
 
 request(urlInput, (error, response, body) => {
-	console.log("error:", error);
-	// Print the error if one occurred
-	console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-	console.log("body:", body); // Print the HTML for the Google homepage.
-	const data = JSON.parse(body);
-	console.log(data);
-	console.log(typeof data);
-	if (data[0]) {
-		console.log(data[0].description);
-	} else {
-		console.log(`cat breed not found`);
-	}
-	if (!error) {
-		fs.writeFile(path, body, "utf8", request, (err) => {
-			console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
-			if (err) throw err;
-		});
-	}
+  console.log("error:", error);
+  // Print the error if one occurred
+  console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+  console.log("body:", body); // Print the HTML for the Google homepage.
+  const data = JSON.parse(body);
+  console.log(data);
+  console.log(typeof data);
+  if (data[0]) {
+    console.log(data[0].description);
+  } else {
+    console.log(`cat breed not found`);
+  }
+  if (!error) {
+    fs.writeFile(path, body, "utf8", request, (err) => {
+      console.log(`Downloaded and saved ${body.length} bytes to ${path}`);
+      if (err) throw err;
+    });
+  }
 });
